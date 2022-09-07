8c69cad16e67b3ec4421661139da2243d420d893
Guillermo <guillermo.arena@gmail.com>
2022-09-07 18:51:52 -0300

    cambios por el clever

--- gda/src/main/java/com/portfolio/gda/Security/jwt/JwtProvider.java
+++ gda/src/main/java/com/portfolio/gda/Security/jwt/JwtProvider.java
@@ -10,12 +10,14 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import java.security.Key;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import static io.jsonwebtoken.security.Keys.secretKeyFor;
 
 /**
  *
@@ -25,8 +27,11 @@ import org.springframework.stereotype.Component;
 public class JwtProvider {
     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
     
+    
+    
     @Value("${jwt.secret}")
-    private String secret;
+    private String key;
+            
     @Value("${jwt.expiration}")
     private int expiration;
     
@@ -35,17 +40,17 @@ public class JwtProvider {
         return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime()+expiration*1000))
-                .signWith(SignatureAlgorithm.HS512, secret)
+                .signWith(SignatureAlgorithm.HS512,key) // el problema lo tenes aqui. no encrypta la contraseña
                 .compact();
     }
     
     public String getNombreUSuarioFromToken(String token){
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
     }
     
     public boolean validateToken(String token){
         try{
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(key).parseClaimsJws(token);
             return true;
         }catch (MalformedJwtException e){
             logger.error("Token mal formado");
